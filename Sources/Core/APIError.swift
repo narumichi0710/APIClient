31ed63a9d0de3e038e5702666bc7e08fd51cecec
narumichikubo <narumichi7373@gmail.com>
2023-04-05 22:09:10 +0900

    [update] APIErrorのcaseを追加

--- Sources/Core/APIError.swift
+++ Sources/Core/APIError.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  APIError.swift
 //  
 //
 //  Created by Narumichi Kubo on 2023/04/04.
@@ -8,12 +8,21 @@
 import Foundation
 
 public enum APIError: Error, Equatable {
+    case invalidURL
+    case missingAccessToken
+    case unavailableAuthSession
     case serverError
     case noResponse
     case other(String)
 
     var localize: String? {
         switch self {
+        case .invalidURL:
+            return "無効なURLです"
+        case .missingAccessToken:
+            return "アクセストークンがありません"
+        case .unavailableAuthSession:
+            return "認証に失敗しました"
         case .serverError:
             return "サーバーエラーが発生しました。"
         case .noResponse:
