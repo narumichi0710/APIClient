222253a15863bc44f68aa4ec5d8dbc3d35693fd2
narumichikubo <narumichi7373@gmail.com>
2023-05-23 15:45:19 +0900

    APIErrorのcaseを更新

--- Sources/APIClient/APIError.swift
+++ Sources/APIClient/APIError.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-public enum APIError: Error, Equatable {
+public enum APIError: Error {
     case invalidURL
     case missingAccessToken
     case unavailableAuthSession
-    case serverError
     case noResponse
-    case other(String)
+    case serverError(String)
+    case decodingError(String)
 
-    var localize: String? {
+    public var localize: String? {
         switch self {
         case .invalidURL:
             return "無効なURLです"
@@ -23,35 +23,12 @@ public enum APIError: Error, Equatable {
             return "アクセストークンがありません"
         case .unavailableAuthSession:
             return "認証に失敗しました"
-        case .serverError:
-            return "サーバーエラーが発生しました。"
         case .noResponse:
             return "サーバーからの応答がありません。少し時間を置いてからアクセスしてください。"
-        default:
-            return nil
+        case .serverError(let error):
+            return "サーバーエラーが発生しました。\n\(error)"
+        case .decodingError(let error):
+            return "予期せぬエラーが発生しました。\n\(error)"
         }
     }
 }
-
-extension APIError {
-    public struct Message: Decodable, Equatable {
-        public let documentationURL: URL
-        public let errors: Errors
-        public let message: String
-
-        public struct Errors: Decodable, Equatable {
-            public let resource: String
-            public let field: String
-            public let code: String
-
-            private enum CodingKeys: String, CodingKey {
-                case resource, field, code
-            }
-        }
-        private enum CodingKeys: String, CodingKey {
-            case documentationURL = "documentation_url"
-            case errors, message
-        }
-    }
-}
-
